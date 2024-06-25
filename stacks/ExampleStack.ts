a15a8abd5e75c93255aaf3cc58a93596b5c317f9
Evan Kohilas <ekohilas@users.noreply.github.com>
2024-06-25 22:41:00 +1000

    initial framework commit

---
+++ stacks/ExampleStack.ts
@@ -0,0 +1,14 @@
+import { Api, StackContext } from "sst/constructs";
+
+export function ExampleStack({ stack }: StackContext) {
+  // Create the HTTP API
+  const api = new Api(stack, "Api", {
+    routes: {
+    },
+  });
+
+  // Show the API endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+}
