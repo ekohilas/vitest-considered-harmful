7d168f468ad206713a5fa39a178dbf805f41d836
Evan Kohilas <ekohilas@users.noreply.github.com>
2024-06-26 10:34:41 +1000

    added payment service tests using spies

---
+++ packages/functions/tests/paymentService.test.ts
@@ -0,0 +1,32 @@
+import { describe, it, vi } from "vitest";
+import { PaymentService } from "../src/paymentService";
+import { type Payment, PaymentStore } from "../src/paymentStore";
+
+const testPayment: Payment = {
+  id: "000000",
+  amount: 0,
+  description: "Test payment for retrieval in tests",
+};
+const testPayments = [testPayment];
+
+describe("PaymentService", () => {
+  it("checks getting all notes", async ({ expect }) => {
+    vi.spyOn(PaymentStore, "getPaymentsFromStore").mockResolvedValue(
+      testPayments
+    );
+
+    const actualPayments = await PaymentService.getAllPayments();
+
+    expect(actualPayments).toEqual(testPayments);
+  });
+
+  it("checks getting a note via spying", async ({ expect }) => {
+    vi.spyOn(PaymentStore, "getPaymentsFromStore").mockResolvedValue(
+      testPayments
+    );
+
+    const actualNote = await PaymentService.findPayment(testPayment.id);
+
+    expect(actualNote).toEqual(testPayment);
+  });
+});
